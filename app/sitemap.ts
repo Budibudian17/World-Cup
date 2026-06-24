7439ed41afa5e06dcebc3e36fbdf8a628ea33e60
Hilmi Farrel <gitlabprivate@gmail.com>
2026-06-24 15:39:54 +0700

    Add Site Map, Formation Fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.worldcupfanhub.xyz/'
+  const baseUrl = 'https://www.worldcupfanhub.xyz'
 
   return [
     {
